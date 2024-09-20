ec81e576a8f0e5e8f7c4121c8935d40eef4a42f2
Higor Tadeu <higorse@gmail.com>
2024-09-19 22:59:29 -0300

    Criando Configuração de Conexão e Migrations tabela Administradores

--- Infraestrutura/DB/DbContexto.cs
+++ Infraestrutura/DB/DbContexto.cs
@@ -1,6 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.Entidade;
+
 namespace MinimalApi.Infraestrutura.Db;
 
-public class DbContexto
+public class DbContexto : DbContext
 {
-    
+    private readonly IConfiguration _configuracaoAppSettings;
+    public DbContexto(IConfiguration configuracaoAppSettings)
+    {
+       _configuracaoAppSettings = configuracaoAppSettings;
+    }
+    public DbSet<Administrador> Administradores { get; set; } = default!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if(!optionsBuilder.IsConfigured)
+        {
+            var stringConexao = _configuracaoAppSettings.GetConnectionString("mysql")?.ToString();
+            if(!string.IsNullOrEmpty(stringConexao)) //Valida se o valor é Nulo ou Vazio
+            {
+                optionsBuilder.UseMySql(
+                stringConexao,
+                ServerVersion.AutoDetect(stringConexao)
+                );
+            }
+        }   
+        
+    }
 }
